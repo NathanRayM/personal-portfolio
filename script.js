bb0a1ee55678d9105ad182901784eff355cacfbe
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2026-05-01 14:57:03 -0400

    updated accessibility

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ const mobileNavBtn = document.querySelector(".mobile__nav--toggle");
 const mobileNav = document.querySelector(".mobile-nav");
 
 mobileNavBtn.addEventListener("click", () => {
+  const isExpanded = mobileNavBtn.getAttribute("aria-expanded") === "true";
   mobileNav.classList.toggle("hidden");
+  mobileNavBtn.setAttribute("aria-expanded", !isExpanded);
 });
